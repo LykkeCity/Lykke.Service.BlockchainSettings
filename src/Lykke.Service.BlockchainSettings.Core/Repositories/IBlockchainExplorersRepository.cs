ff687d185a2207ca89abb6329ec9a5e94bccc331
AleksandrBelousov <tortyt1@gmail.com>
2019-02-12 19:43:25 +0400

    Created interfaces for blockchains url feature

---
+++ src/Lykke.Service.BlockchainSettings.Core/Repositories/IBlockchainExplorersRepository.cs
@@ -0,0 +1,23 @@
+﻿using Lykke.Service.BlockchainSettings.Core.Domain.Settings;
+using Lykke.Service.BlockchainSettings.Core.Exceptions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lykke.Service.BlockchainSettings.Core.Repositories
+{
+    public interface IBlockchainExplorersRepository
+    {
+        Task<(IEnumerable<BlockchainExplorer>, string continuationToken)> GetAllAsync(int take, string continuationToken = null);
+
+        Task<BlockchainExplorer> GetAsync(string type);
+
+        ///<exception cref="DoesNotExistException">when entity does not exist</exception>
+        Task RemoveAsync(string type);
+
+        ///<exception cref="AlreadyExistsException">when entity is already created</exception>
+        Task CreateAsync(BlockchainExplorer settings);
+
+        ///<exception cref="AlreadyUpdatedException">when entity is already updated</exception>
+        Task UpdateAsync(BlockchainExplorer settings);
+    }
+}
---
+++ src/Lykke.Service.BlockchainSettings.Core/Services/IBlockchainExplorersService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Service.BlockchainSettings.Core.Domain.Settings;
+using Lykke.Service.BlockchainSettings.Core.Exceptions;
+
+namespace Lykke.Service.BlockchainSettings.Core.Services
+{
+    public interface IBlockchainExplorersService
+    {
+        Task<(IEnumerable<BlockchainExplorer>, string continuationToken)> GetAllAsync(int take, string continuationToken = null);
+
+        Task<BlockchainExplorer> GetAsync(string type);
+
+        ///<exception cref="DoesNotExistException">when entity does not exist</exception>
+        Task RemoveAsync(string type);
+
+        ///<exception cref="AlreadyExistsException">when entity is already created</exception>
+        Task CreateAsync(BlockchainExplorer settings);
+
+        ///<exception cref="AlreadyUpdatedException">when entity is already updated</exception>
+        Task UpdateAsync(BlockchainExplorer settings);
+    }
+}
